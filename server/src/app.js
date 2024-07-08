496f49bc4748aa8e6827716eb3d4f13f36fd9c2e
ahnaf4D <ahnaftahmid802@gmail.com>
2024-07-08 13:42:11 +0600

    notes updated

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,32 @@
 import express from 'express';
 import morgan from 'morgan';
 import createHttpError from 'http-errors';
+import xssClean from 'xss';
+import { rateLimit } from 'express-rate-limit';
 const app = express();
+const raterLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
+  massage: 'Too many requests from you ip please try again later',
+}); // rate limiter middlware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.body = JSON.parse(
+    JSON.stringify(req.body, (key, value) =>
+      typeof value === 'string' ? xssClean(value) : value
+    )
+  );
+  req.query = JSON.parse(
+    JSON.stringify(req.query, (key, value) =>
+      typeof value === 'string' ? xssClean(value) : value
+    )
+  );
+  next();
+}); // xss package middleware
 app.use(morgan('dev'));
+app.use(raterLimiter);
+
 // import products from './products.json' assert { type: 'json' };
 // custom middlewares
 // const isLoggedIn = (req, res, next) => {

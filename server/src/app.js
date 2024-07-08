05447d0a901fb17736f3d253ea5606cb9f5d1fe9
ahnaf4D <ahnaftahmid802@gmail.com>
2024-07-08 17:11:56 +0600

    code refactor

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import createHttpError from 'http-errors';
 import xssClean from 'xss';
 import { rateLimit } from 'express-rate-limit';
+import { userRouter } from './routers/userRouter.js';
 const app = express();
 const raterLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
@@ -23,36 +24,20 @@ app.use((req, res, next) => {
     )
   );
   next();
-}); // xss package middleware
+});
 app.use(morgan('dev'));
 app.use(raterLimiter);
-// const port = process.env.PORT || 3001;
+// routers
+app.use('/api/users', userRouter);
+// root endpoint below
 app.get(`/`, (req, res) => {
   res.status(200).send({ massage: 'Welcome to the Nexus Server' });
 });
-app.get(`/api/user`, (req, res) => {
-  console.log(`Users authenticate with ${req.body.id}`);
-  res.status(200).send({ massage: 'user profile returned' });
-});
-app.get(`/test`, (req, res) => {
-  res.status(200).send({ massage: 'GET: API working fine' });
-});
-app.post(`/test`, (req, res) => {
-  res.status(200).send({ massage: 'POST: API working fine' });
-});
-app.put(`/test`, (req, res) => {
-  res.status(200).send({ massage: 'PUT: API working fine' });
-});
-app.delete(`/test`, (req, res) => {
-  res.status(200).send({ massage: 'DELETE: API working fine' });
-});
-// client error handling middlewares
+// error handling
 app.use((req, res, next) => {
   next(createHttpError(404, 'Route not found'));
 });
-// http/server error handling middleware -> all the errors
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ success: false, message: err.message });
 });
-
 export default app;

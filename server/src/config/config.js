141ffcb01428865ab39b8dc2a316a504f105b030
ahnaf4D <ahnaftahmid802@gmail.com>
2024-07-12 00:08:04 +0600

    code update

---
+++ server/src/config/config.js
@@ -0,0 +1,4 @@
+const UPLOAD_FILE = 'public/images/users';
+const MAX_FILE_SIZE = 2097152;
+const EXPECTED_FILE_EXT = ['jpg', 'jpeg', 'png'];
+export { UPLOAD_FILE, MAX_FILE_SIZE, EXPECTED_FILE_EXT };
--- server/src/middlewares/uploadFiles.js
+++ server/src/middlewares/uploadFiles.js
@@ -1,17 +1,14 @@
 import createHttpError from 'http-errors';
 import multer from 'multer';
 import path from 'path';
-
-const maxFileSize = Number(process.env.MAX_FILE_SIZE) || 2097152;
-const uploadDir = process.env.UPLOAD_FILE;
-const allowedFileTypes = process.env.EXPECTED_FILE_EXT || [
-  'jpg',
-  'jpeg',
-  'png',
-];
+import {
+  EXPECTED_FILE_EXT,
+  MAX_FILE_SIZE,
+  UPLOAD_FILE,
+} from '../config/config.js';
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir);
+    cb(null, UPLOAD_FILE);
   },
   filename: function (req, file, cb) {
     const extName = path.extname(file.originalname);
@@ -23,14 +20,14 @@ const storage = multer.diskStorage({
 });
 const fileFilter = (req, file, cb) => {
   const extName = path.extname(file.originalname);
-  if (!allowedFileTypes.includes(extName.substring(1))) {
-    return cb(createHttpError(400, 'File type not allowed'));
+  if (!EXPECTED_FILE_EXT.includes(extName.substring(1))) {
+    return cb(new Error('File type not allowed'), false);
   }
   cb(null, true);
 };
 const upload = multer({
   storage: storage,
-  limits: { fileSize: maxFileSize },
+  limits: { fileSize: MAX_FILE_SIZE },
   fileFilter,
 });
 const handleMulterError = (err, req, res, next) => {
